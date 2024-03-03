89d6bbdc3b6a9f4c4e639a1568dc87b5a1c51391
Juancinho <tj14or@gmail.com>
2024-03-03 17:43:05 +0100

    modales

---
+++ proyecto/scripts/scriptPrincipal.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imagenes = document.querySelectorAll('.carruselContenedorImagenes img');
+    const botonIzquierda = document.querySelector('.botonCarruselIzquierda');
+    const botonDerecha = document.querySelector('.botonCarruselDerecha');
+    let indiceImagenActual = 0;
+  
+    // Inicialmente, mostrar solo la primera imagen
+    imagenes.forEach((img, index) => {
+      img.style.display = index === 0 ? 'block' : 'none';
+    });
+  
+    botonDerecha.addEventListener('click', () => {
+      cambiarImagen('derecha');
+    });
+  
+    botonIzquierda.addEventListener('click', () => {
+      cambiarImagen('izquierda');
+    });
+  
+    function cambiarImagen(direccion) {
+      // Ocultar la imagen actual
+      imagenes[indiceImagenActual].style.display = 'none';
+  
+      // Incrementar o decrementar el índice según el botón presionado
+      if (direccion === 'derecha') {
+        indiceImagenActual = (indiceImagenActual + 1) % imagenes.length;
+      } else if (direccion === 'izquierda') {
+        indiceImagenActual = (indiceImagenActual - 1 + imagenes.length) % imagenes.length;
+      }
+  
+      // Mostrar la nueva imagen
+      imagenes[indiceImagenActual].style.display = 'block';
+    }
+  });
+  
