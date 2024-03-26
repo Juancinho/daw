0396ffc621f8561fb4c3686939bdf2783a06b5f1
Juancinho <89271590+Juancinho@users.noreply.github.com>
2024-03-26 19:15:47 +0100

    desplegable

--- proyecto/scripts/scriptRestaurantes.js
+++ proyecto/scripts/scriptRestaurantes.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
-    // Asumiendo que restaurantes.json está estructurado como se describió anteriormente
-    $.getJSON('json/restaurantes.json', function(data) {
+    // Asumiendo que la ruta a restaurantes.json es correcta y accesible
+    $.getJSON('../json/restaurantes.json', function(data) {
         $('.mas-detalles').click(function() {
             // Encuentra el restaurante correspondiente en el JSON
             var nombreRestaurante = $(this).data('restaurante');
             var detallesRestaurante = data.find(restaurante => restaurante.nombre === nombreRestaurante);
 
-            // Construye el HTML para los detalles
-            var htmlDetalles = "<p>" + detallesRestaurante.detalles + "</p><ul>";
-            detallesRestaurante.menu.forEach(function(item) {
-                htmlDetalles += "<li>" + item.item + ": " + item.precio + "</li>";
-            });
-            htmlDetalles += "</ul>";
+            // Comprueba que detallesRestaurante está definido
+            if (detallesRestaurante) {
+                var htmlDetalles = "<p>" + detallesRestaurante.detalles + "</p><ul>";
 
-            // Muestra los detalles debajo del restaurante correspondiente
-            $(this).next('.detalles-restaurante').html(htmlDetalles).slideToggle();
+                // Asegúrate de que detallesRestaurante.menu está definido y es un array
+                if (Array.isArray(detallesRestaurante.menu)) {
+                    detallesRestaurante.menu.forEach(function(item) {
+                        htmlDetalles += "<li>" + item.item + ": " + item.precio + "</li>";
+                    });
+                } else {
+                    console.error("Menu no definido o no es un array");
+                }
+
+                htmlDetalles += "</ul>";
+
+                // Muestra los detalles debajo del restaurante correspondiente
+                $(this).next('.detalles-restaurante').html(htmlDetalles).slideToggle();
+            } else {
+                console.error("Detalles del restaurante no encontrados para: " + nombreRestaurante);
+            }
         });
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Error al cargar el JSON: " + textStatus + ", " + errorThrown);
     });
 });
