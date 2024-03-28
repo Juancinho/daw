ee6bab9c59f989964236c844aaf0e566346464f6
Juancinho <89271590+Juancinho@users.noreply.github.com>
2024-03-28 12:57:15 +0100

    asdf

--- proyecto/scripts/scriptFadeIn.js
+++ proyecto/scripts/scriptFadeIn.js
@@ -4,13 +4,17 @@ $(window).on('scroll', function() {
         let topOfWindow = $(window).scrollTop();
         let windowHeight = $(window).height();
 
-        if (elementPos < topOfWindow + windowHeight - 100) {
+        if (elementPos < topOfWindow + windowHeight - 10) {
             $(this).addClass('start');
         }
     });
 });
 
 $(document).ready(function(){
+    // Activar la animación de fade in en todos los elementos con la clase .fade-in
+    $('.fade-in').css('opacity', 0).each(function(i) {
+        $(this).delay(i * 500).animate({'opacity': 1}, 1000);
+    });
     // Agrega un evento de despliegue suave al submenú
     showMenu=(e)=>{e.preventDefault();$("#submenu").slideToggle()};
     hideMenu=()=>{ $("#submenu").slideUp()};
