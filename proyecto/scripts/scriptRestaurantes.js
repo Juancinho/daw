fbf708a65dfba1d1db738ebf48e4976047704a89
Juancinho <tj14or@gmail.com>
2024-03-30 11:14:28 +0100

    adsf

--- proyecto/scripts/scriptRestaurantes.js
+++ proyecto/scripts/scriptRestaurantes.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
                 htmlDetalles += "</ul>";
 
-                // Muestra los detalles debajo del restaurante correspondiente
+                // Muestra los detalles extendiendo el desplegable
                 $(this).next('.detalles-restaurante').html(htmlDetalles).slideToggle();
             } else {
                 console.error("Detalles del restaurante no encontrados para: " + nombreRestaurante);
